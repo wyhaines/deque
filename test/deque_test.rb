ca42dc3fabd82b2290f7fb1d6c6433a709625deb
Kirk Haines <wyhaines@gmail.com>
2016-09-05 20:16:07 +0900

    Fixes so that the gem builds right, and tests work.

--- test/deque_test.rb
+++ test/deque_test.rb
@@ -5,18 +5,14 @@ class DequeTest < Minitest::Test
         refute_nil ::Deque::VERSION
     end
 
-    def test_it_does_something_useful
-        assert false
-    end
-
     def test_a_new
         dq = nil
-        dq = Swiftcore::Deque.new
-        assert_kind_of(Swiftcore::Deque, dq)
+        dq = Deque.new
+        assert_kind_of(Deque, dq)
     end
 
     def test_b_unshift
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
 
         dq.unshift 'a'
         dq.unshift 'b'
@@ -26,7 +22,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_c_shift
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.unshift 'a'
         dq.unshift 'b'
         dq.unshift 'c'
@@ -37,7 +33,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_d_push
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -46,7 +42,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_e_pop
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -57,7 +53,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_f_size
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -65,12 +61,12 @@ class DequeTest < Minitest::Test
     end
 
     def test_g_max_size
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.max_size
     end
 
     def test_h_clear
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -80,7 +76,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_i_empty
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         assert(!dq.empty?)
         dq.clear
@@ -88,7 +84,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_j_to_s
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -96,7 +92,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_k_to_a
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -104,7 +100,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_l_first
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -112,7 +108,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_m_last
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -120,7 +116,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_n_at
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push 'b'
         dq.push 'c'
@@ -133,7 +129,7 @@ class DequeTest < Minitest::Test
     end
 
     def test_o_index
-        dq = Swiftcore::Deque.new
+        dq = Deque.new
         dq.push 'a'
         dq.push :b
         dq.push 37
